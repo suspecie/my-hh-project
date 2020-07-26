8e65127b3c156ff9761ba8fbc83de451750ed0fc
Su Specie <sspecie13@gmail.com>
2020-07-25 22:00:13 -0300

    feat(header): added link in logo

--- src/app/core/components/header/header.module.ts
+++ src/app/core/components/header/header.module.ts
@@ -1,3 +1,4 @@
+import { RouterModule } from '@angular/router';
 import { HeaderComponent } from './header.component';
 import { NgModule } from '@angular/core';
 
@@ -5,6 +6,7 @@ import { NgModule } from '@angular/core';
   declarations: [
     HeaderComponent,
   ],
+  imports: [RouterModule],
   exports: [
     HeaderComponent,
   ],
