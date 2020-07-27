7e5d86dea64b44ae65180c084282e16052904c65
Su Specie <sspecie13@gmail.com>
2020-07-27 01:15:01 -0300

    feat(breadcrumb): styled breadcrumb

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,4 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   templateUrl: './app.component.html',
 })
-export class AppComponent {
-  title = 'my-hh-project';
-}
+export class AppComponent {}
