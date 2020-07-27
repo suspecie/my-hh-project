30cd8f927cd2bcfb76eca6a5d2b154334f2a1c3e
Su Specie <sspecie13@gmail.com>
2020-07-27 02:19:31 -0300

    feat(products): images opacity and not product founded

--- src/app/shared/components/card-product/card-product.component.ts
+++ src/app/shared/components/card-product/card-product.component.ts
@@ -21,6 +21,8 @@ export class CardProductComponent implements OnInit {
 
   public price = [];
 
+  public isErrorImage = [];
+
   constructor(
     private productsService: ProductsService,
   ) { }
